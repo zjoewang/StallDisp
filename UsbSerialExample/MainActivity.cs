79fa287e55435fc03bed495fb866f09f5dbab181
Joe Wang <todd60young@hotmail.com>
2017-02-24 21:29:10 -0600

    Added SD manifests

--- UsbSerialExample/MainActivity.cs
+++ UsbSerialExample/MainActivity.cs
@@ -29,7 +29,7 @@ namespace ESB
 		static readonly string TAG = typeof(MainActivity).Name;
 		const string ACTION_USB_PERMISSION = "com.hoho.android.usbserial.examples.USB_PERMISSION";
 
-        string build_number = "0.815";
+        string build_number = "0.817";
 
 		UsbManager usbManager;
 		ListView listView;
